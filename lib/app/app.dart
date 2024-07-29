8096a9dc7317beb68bdc24355495c1df3155e8ff
vpknew <66vpk50@gmail.com>
2024-07-28 22:15:39 -0600

    profile

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -2,17 +2,15 @@
 import 'package:flutter/material.dart';
 import 'package:zc_dodiddone/theme/theme.dart';
 
-import '../peges/my_home_page.dart';
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: DoDidDoneTheme.lightTheme,
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
